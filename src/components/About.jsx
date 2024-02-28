ebcdcd917567c89c36aad85ce34b6f01b622891a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-02-28 01:19:31 +0100

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,14 +37,14 @@ const About = () => {
   ];
 
   return (
-    <div className="bg-stone-400 border-b border-stone-500">
-      <div className=" w-[65%] mx-auto">
+    <div className="bg-stone-400 ">
+      <div className="">
         <motion.div
           whileInView={{ scale: [0.95, 1], opacity: [0, 0.3, 1] }}
           transition={{ duration: 1, ease: "easeInOut" }}
           className="header-title"
         >
-          <div className=" flex py-[5vw] justify-between">
+          <div className=" flex py-[5vw] justify-between  w-[65%] mx-auto">
             {items.map((item, index) => (
               <div key={index}>
                 {item.icon}
@@ -53,13 +53,13 @@ const About = () => {
             ))}
           </div>
         </motion.div>
-        <div className="flex justify-center items-center ">
+        <div className="flex justify-center items-center  w-[72%] mx-auto ">
           <motion.div
             whileInView={{ x: [-100, 0], opacity: [0, 1] }}
             transition={{ duration: 1 }}
             className="text"
           >
-            <div className="mr-[-3vw]">
+            <div className="mr-[-4vw]">
               <h6 className="text-[1.3vw] py-2 animate-pulse ">
                 That's me in the office
               </h6>
@@ -85,7 +85,7 @@ const About = () => {
                   </p>
                 </div>
               </div>
-              <div className=" ml-[4vw] px-auto flex gap-10">
+              <div className=" ml-[4.2vw] px-auto flex gap-[1vw]">
                 <div className="flex  ">
                   <div className=" border-white border-[2px]  rounded-full">
                     <p className="text-white font-bold text-[1.4vw] border px-[1.1vw] py-[0.3vw] rounded-full">
@@ -99,7 +99,7 @@ const About = () => {
                   </p>
                 </div>
               </div>
-              <div className="ml-[5vw] px-auto flex gap-10">
+              <div className="ml-[5.2vw] px-auto flex gap-[1vw]">
                 <div className="flex border-white border-[2px]  rounded-full">
                   <p className="text-white font-bold text-[1.4vw] border px-[1.1vw] py-[0.3vw] rounded-full">
                     NEW BUSINESSES
@@ -113,17 +113,17 @@ const About = () => {
                   </div>
                 </div>
               </div>
-              <p className="text-[1.05vw] mt-[1vw] ml-[5vw] ">
+              <p className="text-[1.05vw] mt-[0.7vw] ml-[5.2vw] ">
                 I'm a passionate Full-Stack web developer freelancer,
                 specializing in crafting dynamic user experiences through
                 innovative UI designs and seamless functionality.
               </p>
-              <p className="text-[1.05vw] mt-[1vw] ml-[3vw]">
+              <p className="text-[1.05vw] ml-[3.2vw]">
                 Dedicated to creating top-notch software that enhances lives, I
                 work with a diverse range of clients, from individuals and small
                 businesses to large enterprises.
               </p>
-              <p className="text-[1.05vw] mt-[1vw] ml-[1vw]">
+              <p className="text-[1.05vw] ">
                 Let's work together to bring your digital vision to life!
               </p>
             </div>
@@ -134,7 +134,7 @@ const About = () => {
           transition={{ duration: 1 }}
           className="card"
         >
-          <div className="flex gap-[2vw] justify-between py-[6vw]">
+          <div className="flex gap-[2vw] justify-between py-[6vw]  w-[65%] mx-auto">
             <p className=" text-[0.8vw] border-black border-[2px] font-bold px-[0.5vw] py-[0.1vw] rounded-full">
               HTML
             </p>
@@ -170,35 +170,9 @@ const About = () => {
           </div>
         </motion.div>
       </div>
+      <div className="w-[75%] h-[2.5px] bg-stone-500 mx-auto "></div>
     </div>
   );
 };
 
 export default About;
-
-{
-  /* <IoDiamondOutline className="text-[3vw] mx-auto" />
-            <p className="py-[1vw] text-[1.05vw]">World class Apps</p> */
-}
-
-{
-  /* <div className="">
-            <BsTools className="text-[3vw] mx-auto" />
-            <p className="py-[1vw] text-[1.05vw]">Tailored To Your Needs</p>
-          </div>
-          <div>
-            <MdOutlineSpeed className="text-[3vw] mx-auto" />
-            <p className="py-[1vw] text-[1.05vw]">Fast load times</p>
-          </div>
-          <div>
-            <LuMonitorSmartphone className="text-[3vw] mx-auto" />
-
-            <p className="py-[1vw] text-[1.05vw]">
-              Responsive, work on all devices
-            </p>
-          </div>
-          <div>
-            <GoLightBulb className="text-[3vw] mx-auto" />
-            <p className="py-[1vw] text-[1.05vw]">Intuitive and dynamic</p>
-          </div> */
-}
